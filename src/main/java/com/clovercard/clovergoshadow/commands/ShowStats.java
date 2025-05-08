c06ec6b35d28bcec6917ef7d95c65e707c829677
PlatanoV3rde <franciscojaviercom59@gmail.com>
2025-05-08 17:40:15 -0500

    Update ShowStats.java

--- src/main/java/com/clovercard/clovergoshadow/commands/ShowStats.java
+++ src/main/java/com/clovercard/clovergoshadow/commands/ShowStats.java
@@ -13,6 +13,7 @@ import net.minecraft.scoreboard.ServerScoreboard;
 import net.minecraft.util.Util;
 import net.minecraft.util.text.IFormattableTextComponent;
 import net.minecraft.util.text.StringTextComponent;
+import net.minecraft.util.text.TextFormatting;
 import net.minecraft.util.text.TranslationTextComponent;
 import net.minecraftforge.fml.server.ServerLifecycleHooks;
 
@@ -24,6 +25,7 @@ public class ShowStats {
                                 .executes(cmd -> getPlayerResearch(cmd.getSource())))
         );
     }
+    
     public int getPlayerResearch(CommandSource src) {
         if(!(src.getEntity() instanceof ServerPlayerEntity)) return 1;
         ServerPlayerEntity player = (ServerPlayerEntity) src.getEntity();
@@ -42,13 +44,37 @@ public class ShowStats {
         if(levelObj == null || expObj == null) {
             return 1;
         }
+        
         Score plLevel = scoreboard.getOrCreatePlayerScore(player.getName().getString(), levelObj);
         Score plExp = scoreboard.getOrCreatePlayerScore(player.getName().getString(), expObj);
         IFormattableTextComponent msg;
+        
         if(Config.CONFIG.isUseTranslatables()) {
             msg = new TranslationTextComponent("clovergoshadow.displaystats", plLevel.getScore(), (ExpHelper.getLevelExp(plLevel.getScore()) - plExp.getScore()));
         }
-        else msg = new StringTextComponent("Your current research level is " + plLevel.getScore() + "! You need " + (ExpHelper.getLevelExp(plLevel.getScore()) - plExp.getScore()) + " exp to level up!");
+        else {
+            // Crear mensaje con colores
+            IFormattableTextComponent baseMsg = new StringTextComponent("Tu nivel de investigación actual es ");
+            
+            // Nivel en color dorado
+            IFormattableTextComponent levelPart = new StringTextComponent(String.valueOf(plLevel.getScore()));
+            levelPart.setStyle(levelPart.getStyle().applyFormat(TextFormatting.GOLD));
+            
+            // Texto intermedio
+            IFormattableTextComponent middlePart = new StringTextComponent("! Necesitas ");
+            
+            // Experiencia necesaria en color verde
+            int expNeeded = ExpHelper.getLevelExp(plLevel.getScore()) - plExp.getScore();
+            IFormattableTextComponent expPart = new StringTextComponent(String.valueOf(expNeeded));
+            expPart.setStyle(expPart.getStyle().applyFormat(TextFormatting.GREEN));
+            
+            // Texto final
+            IFormattableTextComponent endPart = new StringTextComponent(" puntos de experiencia para subir de nivel!");
+            
+            // Construir mensaje completo
+            msg = baseMsg.append(levelPart).append(middlePart).append(expPart).append(endPart);
+        }
+        
         player.sendMessage(msg, Util.NIL_UUID);
         return 0;
     }
