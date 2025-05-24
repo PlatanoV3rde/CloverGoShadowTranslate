e5be5f92b6a409dfceca8e0e3183aaa9056e9562
PlatanoV3rde <franciscojaviercom59@gmail.com>
2025-05-24 04:52:41 -0500

    Update RibbonEquipped.java

--- src/main/java/com/clovercard/clovergoshadow/listeners/RibbonEquipped.java
+++ src/main/java/com/clovercard/clovergoshadow/listeners/RibbonEquipped.java
@@ -4,45 +4,23 @@ import com.clovercard.clovergoshadow.config.Config;
 import com.clovercard.clovergoshadow.enums.RibbonEnum;
 import com.clovercard.clovergoshadow.helpers.RibbonHelper;
 import com.pixelmonmod.api.registry.RegistryValue;
-import com.pixelmonmod.pixelmon.api.events.PokemonSendOutEvent;
-import com.pixelmonmod.pixelmon.api.pokemon.ribbon.Ribbon;
+import com.pixelmonmod.pixelmon.api.pokemon.ribbon.event.RibbonEvent;
 import com.pixelmonmod.pixelmon.api.pokemon.ribbon.type.RibbonType;
 import net.minecraft.util.text.StringTextComponent;
-import net.minecraft.util.text.Style;
-import net.minecraft.util.text.TextFormatting;
-import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
-public class SendOutPokemon {
-    @SubscribeEvent(priority = EventPriority.HIGH)
-    public void onSendOut(PokemonSendOutEvent event) {
-        if (Config.CONFIG.isUseTranslatables()) return;
-
-        Ribbon ribbon = event.getPokemon().getDisplayedRibbon();
-
-        if (ribbon == null) {
-            return;
-        }
-
+public class RibbonEquipped {
+    @SubscribeEvent
+    public void ribbonEquipped(RibbonEvent.SetDisplayedRibbon event) {
+        if(Config.CONFIG.isUseTranslatables()) return;
         RibbonType shadow = RibbonHelper.getRibbonTypeIfExists(RibbonEnum.SHADOW_RIBBON.getRibbonId());
         RibbonType purified = RibbonHelper.getRibbonTypeIfExists(RibbonEnum.PURIFIED_RIBBON.getRibbonId());
-        if (shadow == null || purified == null) return;
-
-        RegistryValue<RibbonType> reg = ribbon.getType();
-        if (!reg.getValue().isPresent()) return;
-
+        if(shadow == null || purified == null) return;
+        if(event.getRibbon() == null) return;
+        RegistryValue<RibbonType> reg = event.getRibbon().getType();
+        if(!reg.getValue().isPresent()) return;
         RibbonType equipped = reg.getValueUnsafe();
-
-        if (equipped.equals(shadow)) {
-            ribbon.getRibbonData().setSuffix(
-                new StringTextComponent(" Oscuro").setStyle(Style.EMPTY.withColor(TextFormatting.DARK_PURPLE))
-            );
-        } else if (equipped.equals(purified)) {
-            ribbon.getRibbonData().setSuffix(
-                new StringTextComponent(" Purificado").setStyle(Style.EMPTY.withColor(TextFormatting.AQUA))
-            );
-        } else {
-            ribbon.getRibbonData().setSuffix(StringTextComponent.EMPTY);
-        }
+        if(equipped.equals(shadow)) event.getRibbon().getRibbonData().setPrefix(new StringTextComponent("Oscuro "));
+        if(equipped.equals(purified)) event.getRibbon().getRibbonData().setPrefix(new StringTextComponent("Purificado "));
     }
 }
